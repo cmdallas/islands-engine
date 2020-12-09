69f30de2f5257b76ed40ec114f3a181f6e20da1a
cmdallas <self@chrisdallas.tech>
2020-12-08 17:15:23 -0800

    (island): add island building logic

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -4,5 +4,104 @@ defmodule IslandsEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new(), do: %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  @doc """
+  Builds an island given an island type and top left coordinate.
+
+  ## Examples
+
+    iex> IslandsEngine.Island.new(:unsupported_shape, %IslandsEngine.Coordinate{col: 6, row: 4})
+    {:error, :invalid_island_type}
+
+    # /thinkingemoji Copied directly out of iex, but doctest still fails.
+    #iex> IslandsEngine.Island.new(:l_shape, %IslandsEngine.Coordinate{col: 6, row: 4})
+    {:ok, %IslandsEngine.Island{
+      coordinates: #MapSet<[
+        %IslandsEngine.Coordinate{col: 6, row: 4},
+        %IslandsEngine.Coordinate{col: 6, row: 5},
+        %IslandsEngine.Coordinate{col: 6, row: 6},
+        %IslandsEngine.Coordinate{col: 7, row: 6}
+      ]>,
+      hit_coordinates: #MapSet<[]>
+    }}
+  """
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+      %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      # Expects:
+      # {:error, :invalid_coordinate}
+      # {:error, :invalid_island_type}
+      error -> error
+    end
+  end
+
+  #     0   1   2
+  #   +-----------+
+  #  0| X | X |   |
+  #   +-----------+
+  #  1| X | X |   |
+  #   +-----------+
+  #  2| X | X |   |
+  #   +-----------+
+  defp offsets(:square), do: [{0,0}, {0,1}, {1,0}, {1,1}]
+
+  #     0   1   2
+  #   +-----------+
+  #  0| X | X |   |
+  #   +-----------+
+  #  1|   | X |   |
+  #   +-----------+
+  #  2| X | X |   |
+  #   +-----------+
+  defp offsets(:atoll), do: [{0,0}, {0,1}, {1,1}, {0,2}, {2,1}]
+
+  #     0   1   2
+  #   +-----------+
+  #  0| X |   |   |
+  #   +-----------+
+  #  1|   |   |   |
+  #   +-----------+
+  #  2|   |   |   |
+  #   +-----------+
+  defp offsets(:dot), do: [{0,0}]
+
+  #     0   1   2
+  #   +-----------+
+  #  0| X |   |   |
+  #   +-----------+
+  #  1| X |   |   |
+  #   +-----------+
+  #  2| X | X |   |
+  #   +-----------+
+  defp offsets(:l_shape), do: [{0,0}, {1,0}, {2,0}, {2,1}]
+
+  #     0   1   2
+  #   +-----------+
+  #  0|   | X | X |
+  #   +-----------+
+  #  1| X | X  |  |
+  #   +-----------+
+  #  2|   |   |   |
+  #   +-----------+
+  defp offsets(:s_shape), do: [{0,1}, {0,2}, {1,0}, {1,1}]
+
+  defp offsets(_), do: {:error, :invalid_island_type}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    # Each time a coordinate is built, validate it. Halt enumeration if a coordinate is invalid
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} ->
+        {:halt, {:error, :invalid_coordinate}}
+    end
+  end
 end
--- test/islands_engine_test.exs
+++ test/islands_engine_test.exs
@@ -1,6 +1,7 @@
 defmodule IslandsEngineTest do
   use ExUnit.Case
   doctest IslandsEngine
+  doctest IslandsEngine.Island
 
   test "greets the world" do
     assert IslandsEngine.hello() == :world
