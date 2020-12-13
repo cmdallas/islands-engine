2c18f34642eb240eedeaf2920d65bfa3bbcc2fb8
cmdallas <self@chrisdallas.tech>
2020-12-13 11:35:41 -0800

    (game): add a new player

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -46,4 +46,32 @@ defmodule IslandsEngine.Game do
   def start_link(name) when is_binary(name), do:
     GenServer.start_link(__MODULE__, name, [])
 
+  @doc """
+    iex> alias IslandsEngine.Game
+    iex> {:ok, game} = Game.start_link("Chris")
+    iex> Game.add_player(game, "Alexis")
+    iex> state = :sys.get_state(game)
+    iex> state.player2.name
+    "Alexis"
+  """
+  def add_player(game, name) when is_binary(name), do:
+    GenServer.call(game, {:add_player, name})
+
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player) do
+      state
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state}
+    end
+  end
+
+  defp update_player2_name(state, name), do:
+    put_in(state.player2.name, name)
+
+  defp update_rules(state, rules), do: %{state | rules: rules}
+
+  defp reply_success(state, reply), do: {:reply, reply, state}
 end
